91e1b6657b0c72a90a844ef3bb20ddff77453847
Eldar <ekhismatov@list.ru>
2023-09-27 11:21:56 +0500

    Change test

--- src/test/java/PostmanEchoTests.java
+++ src/test/java/PostmanEchoTests.java
@@ -3,87 +3,21 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
-public class PostmanEchoTests {
+class MobileBankApiTestV1 {
     @Test
-    void shouldTestSomeData() {
+    void shouldReturnDemoAccounts() {
+// Given - When - Then
+// Предусловия
         given()
                 .baseUri("https://postman-echo.com")
-                .contentType("text/plain; charset=UTF-8")
-                .body("Good Day")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
                 .when()
                 .post("/post")
+// Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Good Day"))
+                .body("data", equalTo("some data"))
         ;
-    }
-
-    @Test
-    void shouldTestGetArgsFoo1Value() {
-        given()
-                .baseUri("https://postman-echo.com")
-                .contentType("text/plain; charset=UTF-8")
-                .when()
-                .get("/get?foo1=bar1&foo2=bar2")
-                .then()
-                .statusCode(200)
-                .body("args.foo1", equalTo("bar1"))
-        ;
-    }
-
-    @Test
-    void shouldTestGetArgsFoo2Value() {
-        given()
-                .baseUri("https://postman-echo.com")
-                .contentType("text/plain; charset=UTF-8")
-                .body("some data")
-                .when()
-                .get("/get?foo1=bar1&foo2=bar2")
-                .then()
-                .statusCode(200)
-                .body("args.foo2", equalTo("bar2"))
-        ;
-    }
-
-    @Test
-    void shouldTestPost() {
-        given()
-                .baseUri("https://postman-echo.com")
-                .body("handWave")
-                .when()
-                .post("/post")
-                .then()
-                .statusCode(200)
-                .body("data", equalTo("handWave"))
-
-        ;
-    }
-
-    @Test
-    void shouldRequestHeaders() {
-        given()
-                .baseUri("https://postman-echo.com")
-                .when()
-                .get("/headers")
-                .then()
-                .statusCode(200)
-                .headers("Connection", "keep-alive",
-                        "Content-Type", "application/json; charset=utf-8");
-
-    }
-
-    @Test
-    void shouldRequestBasicAuth() {
-        given()
-                .baseUri("https://postman-echo.com")
-                .headers("Authorization","Basic cG9zdG1hbjpwYXNzd29yZA==")
-                .when()
-                .get("/basic-auth")
-
-                .then()
-                .statusCode(200)
-                .body("authenticated", equalTo(true))
-        ;
-
     }
 }
